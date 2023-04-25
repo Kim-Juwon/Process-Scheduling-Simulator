4f2f4392e759198f4306248db48128b7876b6979
김주원 <damiano102777@naver.com>
2023-04-25 22:04:55 +0900

    refactor: request 제약조건 수정

--- src/main/java/com/pssimulator/dto/request/ProcessRequestDto.java
+++ src/main/java/com/pssimulator/dto/request/ProcessRequestDto.java
@@ -8,13 +8,13 @@ import javax.validation.constraints.Positive;
 
 @Getter
 public class ProcessRequestDto {
-    @NotBlank(message = "이름은 필수이며, 길이가 0이거나 공백 문자로만 이루어져 있으면 안됩니다.")
+    @NotBlank(message = "프로세스의 이름은 필수이며, 길이가 0이거나 공백 문자로만 이루어져 있으면 안됩니다.")
     private String name;
 
     @NotNull(message = "프로세스의 arrival time은 필수입니다.")
     private Integer arrivalTime;
 
     @NotNull(message = "프로세스의 workload는 필수입니다.")
-    @Positive(message = "workload는 1 이상이어야 합니다.")
+    @Positive(message = "프로세스의 workload는 1 이상이어야 합니다.")
     private Integer workload;
 }
--- src/main/java/com/pssimulator/dto/request/ProcessorRequestDto.java
+++ src/main/java/com/pssimulator/dto/request/ProcessorRequestDto.java
@@ -2,11 +2,12 @@ package com.pssimulator.dto.request;
 
 import lombok.Getter;
 
+import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
 
 @Getter
 public class ProcessorRequestDto {
-    @NotNull(message = "프로세서의 이름은 필수입니다.")
+    @NotBlank(message = "프로세서의 이름은 필수이며, 길이가 0이거나 공백 문자로만 이루어져 있으면 안됩니다.")
     private String name;
 
     @NotNull(message = "프로세서의 코어 정보는 필수입니다.")
