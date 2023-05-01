a2af7ecbca261f80d00fc0c6ebc9ac84b13f45ab
김주원 <damiano102777@naver.com>
2023-05-01 15:38:02 +0900

    feat: cors 설정

--- src/main/java/com/pssimulator/controller/SchedulingController.java
+++ src/main/java/com/pssimulator/controller/SchedulingController.java
@@ -4,12 +4,14 @@ import com.pssimulator.dto.request.Request;
 import com.pssimulator.dto.response.Response;
 import com.pssimulator.service.SchedulingService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.validation.Valid;
 
+@CrossOrigin(origins = "*")
 @RestController
 @RequiredArgsConstructor
 public class SchedulingController {
