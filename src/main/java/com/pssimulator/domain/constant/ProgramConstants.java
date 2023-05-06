a1a42195fafae0967db69d8aa4d7f87eb3e43121
김주원 <damiano102777@naver.com>
2023-05-07 00:13:49 +0900

    refactor: 프로세서 최대 개수 변경

--- src/main/java/com/pssimulator/domain/constant/ProgramConstants.java
+++ src/main/java/com/pssimulator/domain/constant/ProgramConstants.java
@@ -2,6 +2,6 @@ package com.pssimulator.domain.constant;
 
 public class ProgramConstants {
     public static final int MAX_PROCESSES_SIZE = 99;
-    public static final int MAX_PROCESSORS_SIZE = 4;
+    public static final int MAX_PROCESSORS_SIZE = 15;
     public static final double REMAINING_WORKLOAD_BASELINE_RATIO = 0.1;
 }
