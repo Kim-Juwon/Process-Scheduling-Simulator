a68867ed35c019726e2789d37e452d3343b3ede4
김주원 <damiano102777@naver.com>
2023-05-08 23:00:47 +0900

    refactor: Preemptible 인터페이스 주석 오타 수정

--- src/main/java/com/pssimulator/domain/queue/Preemptible.java
+++ src/main/java/com/pssimulator/domain/queue/Preemptible.java
@@ -3,7 +3,7 @@ package com.pssimulator.domain.queue;
 import com.pssimulator.domain.process.Processes;
 
 /**
- *  선점 알고리즘 스케줄러에서 사용하는 queue에서 구현해야 하는 method
+ *  선점 알고리즘 스케줄러에서 사용하는 queue에서 구현해야 하는 interface
  */
 public interface Preemptible {
     void addPreemptedProcesses(Processes preemptedProcesses);
